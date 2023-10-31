14d634a4cb88c928ab80b2432d82f079ab5897c6
erenyeag239 <147978285+erenyeag239@users.noreply.github.com>
2023-10-31 09:07:46 +0100

    Update config.js

--- config.js
+++ config.js
@@ -44,7 +44,7 @@ module.exports = {
     APP_NAME: process.env.HEROKU_APP_NAME === undefined ? 'kelvin12' : process.env.HEROKU_APP_NAME
 },
   VERSION: process.env.VERSION === undefined ? 'v.0.0.3' : process.env.VERSION,
-  LANG: process.env.THEME|| 'Garena'
+  LANG: process.env.THEME|| 'GARENA'
   WORKTYPE: process.env.WORKTYPE === undefined ? 'private' : process.env.WORKTYPE
 };
 
